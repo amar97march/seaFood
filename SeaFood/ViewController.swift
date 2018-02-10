616b8cc80ce6f0526545130b8868fffc0fb6469e
Amar Singh <amar97march@gmail.com>
2018-02-10 12:53:26 +0530

    CoreML incorporated and now able to get the recognised data back

--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -25,10 +25,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
-        imageView.image = userPickedImage
+            imageView.image = userPickedImage
+            
+            guard let ciimage = CIImage(image : userPickedImage) else {
+                fatalError("Could not convert to ciimage")
+            }
+        
+            detect(image: ciimage)
         }
+        
         imagePicker.dismiss(animated: true, completion: nil)
     }
+    
+    func detect(image : CIImage){
+        
+        guard let model = try? VNCoreMLModel(for : Inceptionv3().model) else {
+            fatalError("Loading CoreML Model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let results = request.results as? [VNClassificationObservation] else {fatalError("Model failed to process image")}
+            //print(results)
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage : image)
+        
+        do {
+        try handler.perform([request])
+        }catch{
+            print(error)
+        }
+    }
+    
+    
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true,completion: nil)
